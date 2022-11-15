9c1331c5d08022938934e985897bce368bc6ef40
NarcisseKOTZ <mansilland@gmail.com>
2022-11-15 16:27:04 -0300

    Primera prueba solo

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -8,7 +8,7 @@ function devolverString(str) {
     // "Return" la string provista: str
     // Tu código:
   }
-  djghfkds
+  
   function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
